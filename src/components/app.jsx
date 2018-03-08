84a5a848698253fc106902c6d6d4bfef32024748
akrst <akirasato@psychic-vr-lab.com>
2018-03-08 22:31:15 +0900

    GoogleMapの導入終了

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,31 +1,77 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
-import Greeting from './greeting.jsx';
+import SearchForm from './SearchForm.jsx';
+import GeocodeResult from './GeocodeResult.jsx';
+import Map from './Map.jsx';
+
+const GEOCODE_ENDPOINT ='https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			name: 'John',
 		};
 	}
-	handleNameChange(name){
-		this.setState({name}); //ショートハンド name: name の省略
+	setErrorMessage (message){
+		this.setState({
+			address: message,
+			lat: 0,
+			lng: 0,
+		});
 	}
+
+	handlePlaceSubmit(place){
+		axios
+			.get(GEOCODE_ENDPOINT, { params: { address: place } })
+			.then((results) => {
+					console.log(results);
+					const data =results.data;
+					const result = data.results[0];
+					switch(data.status){
+						case 'OK': {
+							const location = result.geometry.location;
+							this.setState({
+								address: result.formatted_address,
+								lat: location.lat,
+								lng: location.lng,
+							});
+							break;
+						}
+						case 'ZERO_RESULTS':{
+							this.setErrorMessage('結果が見つかりませんでした。');
+							break;
+						}
+						default: {
+							this.setErrorMessage('エラーが発生しました。');
+						}
+					}
+			})
+			.catch((error) => {
+				this.setErrorMessage('通信に失敗しました。');
+			});
+	}
+
 
 
 	render(){
 		return(
 			<div>
-				<input
-					type="text"
-					value={this.state.name}
-					onChange={e => this.handleNameChange(e.target.value)}
+				<h1>緯度経度検索</h1>
+				<SearchForm onSubmit={ place => this.handlePlaceSubmit(place)} />
+				<GeocodeResult
+					address={this.state.address}
+					lat={this.state.lat}
+					lng={this.state.lng}
 				/>
-				<Greeting name={this.state.name} />
+				<Map
+					lat={this.state.lat}
+					lng={this.state.lng}
+				/>
 			</div>
 			);
 	}
 }
 
 export default App;
+
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; //ブラウザに特化
 
-import App from './components/app.jsx'
+import App from './components/App.jsx'
 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
